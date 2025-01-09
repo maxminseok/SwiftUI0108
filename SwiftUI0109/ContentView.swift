6adaa82d545ff05891305b48d0da76694285ab6c
minseok <pms950106@gmail.com>
2025-01-09 15:56:26 +0900

    first commit

---
+++ SwiftUI0109/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI0109
+//
+//  Created by t2023-m0072 on 1/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI0109/SwiftUI0109App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI0109App.swift
+//  SwiftUI0109
+//
+//  Created by t2023-m0072 on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI0109App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
