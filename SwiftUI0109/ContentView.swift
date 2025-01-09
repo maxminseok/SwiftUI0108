fe02e39e77489b9bad95e36c4b30a2ebbee36479
minseok <pms950106@gmail.com>
2025-01-09 17:20:32 +0900

    [Chore] border와 간격 추가

--- SwiftUI0109/ContentView.swift
+++ SwiftUI0109/ContentView.swift
@@ -21,13 +21,17 @@ struct ContentView: View {
                     ForEach(text1.indices, id: \.self) { index in
                         Text(text1[index])
                             .foregroundColor(text1[index] == "Hello" ? Color.blue : Color.black)
-                            
+                        if index < text1.count - 1 && text1[index] == "Hello" && text1[index + 1] == "World" { Spacer().frame(height: 10) // 간격 추가
+                        }
                     }
-                    
                 }
+                Spacer()
             }
-            .padding()
-            .background(Color.yellow)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 20)
+            .frame(maxWidth: 300)
+            .background(Color.yellow.edgesIgnoringSafeArea(.all))
+            
             Spacer().frame(height:20)
             
             HStack {
@@ -39,16 +43,20 @@ struct ContentView: View {
                     ForEach(text2.indices, id: \.self) { index in
                         Text(text2[index])
                             .foregroundColor(text2[index] == "Hello" ? Color.blue : Color.black)
-                            
+                        if index < text2.count - 1 && text2[index] == "Hello" && text2[index + 1] == "World" { Spacer().frame(height: 10) // 간격 추가
+                        }
                     }
                     
                 }
+                Spacer()
             }
-            .padding()
-            .background(Color.yellow)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .frame(maxWidth: 300)
+            .background(Color.yellow.edgesIgnoringSafeArea(.all))
         }
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 6))
     }
-    
 }
 
 #Preview {
